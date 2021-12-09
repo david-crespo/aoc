c7e0848ac4cb4b401d314145363370a08867fd29
David Crespo <crespo.dm@gmail.com>
2021-12-08 23:51:36 -0600

    python template

---
+++ 2021/python/template.py
@@ -0,0 +1,30 @@
+sample_input = """
+""".strip()
+
+with open("input/day09.txt") as f:
+    real_input = f.read().strip()
+
+
+def parse(input):
+    out = []
+    for line in input.split("\n"):
+        pass
+    return out
+
+
+def run(input):
+    inp = parse(input)
+    print(inp)
+
+
+def run2(input):
+    inp = parse(input)
+
+
+print("=" * 40)
+run(sample_input)
+# run(real_input)
+
+# print()
+# run2(sample_input)
+# run2(real_input)
