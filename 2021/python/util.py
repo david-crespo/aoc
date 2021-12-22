cac0cff123da83720e36d33ce53df454c567743e
David Crespo <crespo.dm@gmail.com>
2021-12-21 23:26:12 -0600

    2021 day 22

--- 2021/python/util.py
+++ 2021/python/util.py
@@ -1,3 +1,8 @@
 def count(pred_or_value, iterable):
     pred = pred_or_value if callable(pred_or_value) else lambda x: x == pred_or_value
     return sum(1 for i in filter(pred, iterable))
+
+
+def pl(iter):
+    for x in iter:
+        print(x)
