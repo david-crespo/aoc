e4aa1cbfd43152cf231b1a1fe29957150ff9b45e
David Crespo <david.crespo@oxidecomputer.com>
2022-11-30 16:38:00 -0600

    2021 day 1 in haskell as a warmup

---
+++ 2021/haskell/day01.hs
@@ -0,0 +1,23 @@
+import Data.Text qualified as T
+
+readInt :: String -> Int
+readInt = read
+
+part1 :: IO ()
+part1 = do
+  input <- readFile "input/day01.txt"
+  let nums = map readInt (lines input)
+  let pairs = zip nums (tail nums)
+  print $ length $ filter (uncurry (<)) pairs
+
+sum3 (a, b, c) = a + b + c
+
+part2 :: IO ()
+part2 = do
+  input <- readFile "input/day01.txt"
+  let nums = map readInt (lines input)
+  let sums = map sum3 $ zip3 nums (tail nums) (tail (tail nums))
+  let pairs = zip sums (tail sums)
+  print $ length $ filter (uncurry (<)) pairs
+
+main = part2
