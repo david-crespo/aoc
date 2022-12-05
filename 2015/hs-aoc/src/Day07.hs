250db823c790d837f347392395ba68cfbbc298d7
David Crespo <david.crespo@oxidecomputer.com>
2022-12-05 15:04:45 -0600

    2015 day 7 works in haskell with very different approach. RIP recursion

--- 2015/hs-aoc/src/Day07.hs
+++ 2015/hs-aoc/src/Day07.hs
@@ -4,9 +4,8 @@ import Text.ParserCombinators.Parsec
 import qualified Data.Map as M
 import Data.Either (fromRight)
 import Data.Bits ((.&.), (.|.), shiftR, shiftL, complement)
-import Data.Maybe (fromMaybe)
 import Data.Word (Word16)
-import Data.List (intercalate)
+-- import Data.List (intercalate)
 
 data Node = Wire String | Value Word16
   deriving Show
@@ -22,12 +21,6 @@ data Expr =
 
 type Program = M.Map String Expr
 
-eand n1 n2 w = (w, And n1 n2)
-eor n1 n2 w = (w, Or n1 n2)
-ers n1 n2 w = (w, RShift n1 n2)
-els n1 n2 w = (w, LShift n1 n2)
-enot n w = (w, Not n)
-edirect n w = (w, Direct n)
 
 val :: GenParser Char st Word16
 val = read <$> many1 digit
@@ -43,79 +36,86 @@ node = (Value <$> val) <|> (Wire <$> wire)
 
 expr :: GenParser Char st (String, Expr)
 expr = choice [
-    eand <$> try (node <* string " AND ") <*> node <*> (string " -> " *> wire),
-    eor <$> try (node <* string " OR ") <*> node <*> (string " -> " *> wire),
-    ers <$> try (node <* string " RSHIFT ") <*> int <*> (string " -> " *> wire),
-    els <$> try (node <* string " LSHIFT ") <*> int <*> (string " -> " *> wire),
-    enot <$> (try (string "NOT ") *> node) <*> (string " -> " *> wire),
-    edirect <$> node <*> (string " -> " *> wire)
-  ]
+    binop And <$> try (node <* string " AND ") <*> node <*> (string " -> " *> wire),
+    binop Or <$> try (node <* string " OR ") <*> node <*> (string " -> " *> wire),
+    binop RShift <$> try (node <* string " RSHIFT ") <*> int <*> (string " -> " *> wire),
+    binop LShift <$> try (node <* string " LSHIFT ") <*> int <*> (string " -> " *> wire),
+    monop Not <$> (try (string "NOT ") *> node) <*> (string " -> " *> wire),
+    monop Direct <$> node <*> (string " -> " *> wire)
+  ] where
+    binop op n1 n2 w = (w, op n1 n2)
+    monop op n w = (w, op n)
 
 
 prog :: GenParser Char st [(String, Expr)]
 prog = many (expr <* newline)
 
-dExpr :: Expr
-dExpr = Not (Value 0)
+-- example :: String
+-- example = "123 -> x\n\
+-- \456 -> y\n\
+-- \x AND y -> d\n\
+-- \x OR y -> e\n\
+-- \x LSHIFT 2 -> f\n\
+-- \y RSHIFT 2 -> g\n\
+-- \NOT x -> h\n\
+-- \NOT y -> i\n"
 
-eval :: Program -> Node -> Word16
-eval p (Wire s) = exec p $ fromMaybe dExpr (M.lookup s p)
-eval _ (Value v) = v
+-- A program is done if all its expressions are resolved to number values
+done :: Program -> Bool
+done p = all (doneExpr . snd) (M.toList p) where
+  doneExpr :: Expr -> Bool
+  doneExpr (Direct (Value _)) = True
+  doneExpr _ = False
 
+mapExpr :: (Node -> Node) -> Expr -> Expr
+mapExpr f (And n1 n2) = And (f n1) (f n2)
+mapExpr f (Or n1 n2) = Or (f n1) (f n2)
+mapExpr f (RShift n i) = RShift (f n) i
+mapExpr f (LShift n i) = LShift (f n) i
+mapExpr f (Not n) = Not (f n)
+mapExpr f (Direct n) = Direct (f n)
 
-exec :: Program -> Expr -> Word16
-exec p (And a b) = eval p a .&. eval p b
-exec p (Or a b) = eval p a .|. eval p b
-exec p (LShift a b) = shiftL (eval p a) b
-exec p (RShift a b) = shiftR (eval p a) b
-exec p (Not a) = complement $ eval p a
-exec p (Direct a) = eval p a
+-- go through and resolve the resolveable expressions
+step :: Program -> Program
+step p = M.map stepValue p where
+  -- if we look up the key and find a direct value, replace the key with the value
+  tryReplace :: Node -> Node
+  tryReplace (Wire a) = case M.lookup a p of (Just (Direct (Value v))) -> (Value v)
+                                             _ -> (Wire a)
+  tryReplace x = x
 
-example :: String
-example = "123 -> x\n\
-\456 -> y\n\
-\x AND y -> d\n\
-\x OR y -> e\n\
-\x LSHIFT 2 -> f\n\
-\y RSHIFT 2 -> g\n\
-\NOT x -> h\n\
-\NOT y -> i\n"
+  -- fully resolve expressions where we have all the values
+  stepValue (And (Value a) (Value b)) = Direct (Value (a .&. b))
+  stepValue (Or (Value a) (Value b)) = Direct (Value (a .|. b))
+  stepValue (LShift (Value a) b) = Direct (Value (shiftL a b))
+  stepValue (RShift (Value a) b) = Direct (Value (shiftR a b))
+  stepValue (Not (Value a)) = Direct (Value (complement a))
 
-pp :: Show a => [a] -> IO ()
-pp xs = putStrLn $ intercalate "\n" $ map show xs
+  -- convert wires to their values if possible
+  stepValue e = mapExpr tryReplace e
 
-exclude = ["a", "ma", "lz"]
+-- pp :: Show a => [a] -> IO ()
+-- pp xs = putStrLn $ intercalate "\n" $ map show xs
+
+-- exclude = ["a", "ma", "lz"]
 
 part1 :: IO ()
 part1 = do
   input <- readFile "../input/day07.txt"
-  -- ugh wish I knew how to do this the proper monad way
-  -- let cmds = fromRight [] $ parse prog "(unknown)" input
   let cmds = M.fromList $ fromRight [] $ parse prog "(unknown)" input
+  print $ M.lookup "a" $ until done step cmds
+
   -- let cmds = M.fromList $ fromRight [] $ parse prog "(unknown)" example
+  -- pp $ M.toList cmds
+  -- print ""
 
-  -- by going through the wires one by one (conveniently they get worse in
-  -- alphabetical order), I can see that the time each one takes goes up a lot,
-  -- to the point where by "ds" it's taking many seconds. so while I could just leave 
-  -- it running and see if it ever finishes, it might not every finish. I have to
-  -- figure out how to memoize. I didn't realize the same wire gets used in multiple
-  -- places, so it's probably exponential complexity without memoization
-  let keys = filter (\k -> not (elem k exclude)) $ take 170 $ map fst $ M.toList cmds
-  print keys
-  pp $ map (\k -> k ++ " " ++ show (eval cmds (Wire k))) keys
-  -- print $ eval cmds (Wire "jo")
-  -- print $ eval cmds (Wire "ma")
-  -- print $ eval cmds (Wire "e")
-  -- print $ eval cmds (Wire "f")
-  -- print $ eval cmds (Wire "g")
-  -- print $ eval cmds (Wire "h")
-  -- print $ eval cmds (Wire "i")
-  -- print $ eval cmds (Wire "x")
-  -- print $ eval cmds (Wire "y")
-
--- not -1
+  -- it takes exactly 431 iterations to get there
+  -- print $ M.lookup "a" $ iterate step cmds !! 431
+  -- pp $ M.toList $ until done step cmds
 
 part2 :: IO ()
 part2 = do
   input <- readFile "../input/day07.txt"
-  print $ "part2"
+  let cmds = M.fromList $ fromRight [] $ parse prog "(unknown)" input
+  let newCmds = (M.insert "b" (Direct (Value 16076)) cmds)
+  print $ M.lookup "a" $ until done step newCmds
