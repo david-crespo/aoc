7d4383a8db3ddbea116a955a5a560aa141a3a9ed
David Crespo <david.crespo@oxidecomputer.com>
2022-12-09 00:51:59 -0600

    improvements to haskell run script

--- 2022/hs-aoc/app/Main.hs
+++ 2022/hs-aoc/app/Main.hs
@@ -4,5 +4,7 @@ import Day09 (part1, part2)
 
 main :: IO ()
 main = do
+  putStrLn "\n=============================================="
   part1
   part2
+  putStrLn "==============================================\n"
