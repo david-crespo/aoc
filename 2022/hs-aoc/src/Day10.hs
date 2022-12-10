4aa2d32b8a122033a8b9a2e72c4a25eff803a8e9
David Crespo <david.crespo@oxidecomputer.com>
2022-12-10 01:04:38 -0600

    2022 day 10 haskell cleanup

--- 2022/hs-aoc/src/Day10.hs
+++ 2022/hs-aoc/src/Day10.hs
@@ -6,19 +6,15 @@ data Op = Noop | Addx Int deriving (Show, Eq)
 
 parseOp :: String -> Op
 parseOp "noop" = Noop
-parseOp s = Addx $ read $ words s !! 1
+parseOp s = (Addx . read . (!! 1) . words) s
 
 type State = (Int, Int) -- (X, cycle)
 
-run :: State -> Op -> State
-run (x, c) Noop = (x, c + 1)
-run (x, c) (Addx a)  = (x + a, c + 2)
-
-getCycles :: [Op] -> [State]
-getCycles cmds = concatMap fillIn (zip states (tail states)) where
-  states = scanl run (1, 1) (cmds ++ [Noop])
-  -- fill in gaps where the adds were taking two cycles
-  fillIn ((ax, ac),(_, bc)) = if bc - ac == 2 then [(ax, ac),(ax, ac+1)] else [(ax, ac)]
+run :: [State] -> Op -> [State]
+run states op = states ++ run' op where
+  (x, c) = last states
+  run' Noop = [(x, c+1)]
+  run' (Addx a) = [(x, c+1), (x+a, c+2)]
 
 draw :: State -> Char
 draw (x, c) = if abs (((c `mod` 40)-1)-x) <= 1 then '#' else '.'
@@ -28,7 +24,8 @@ part1 = do
   input <- readFile "../input/day10.txt"
   -- input <- readFile "../input/day10-example.txt"
   let cmds = map parseOp $ lines input
-  let justVals = map fst (getCycles cmds)
+  let cycles = foldl run [(1, 1)] cmds
+  let justVals = map fst cycles
   print $ sum $ map (\i -> i * (justVals !! (i-1))) [20,60..220]
 
 part2 :: IO ()
@@ -36,4 +33,5 @@ part2 = do
   input <- readFile "../input/day10.txt"
   -- input <- readFile "../input/day10-example.txt"
   let cmds = map parseOp $ lines input
-  putStrLn $ unlines $ chunksOf 40 $ map draw $ getCycles cmds
+  let cycles = foldl run [(1, 1)] cmds
+  putStrLn $ unlines $ chunksOf 40 $ map draw $ cycles
