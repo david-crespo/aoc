f5f1f7ff16a52cb3c74b9d7879b4b4c53b3819af
David Crespo <david.crespo@oxidecomputer.com>
2022-12-26 00:04:24 -0500

    finally day 19 part 1 fast enough to run, and correct

--- 2022/python/day19.py
+++ 2022/python/day19.py
@@ -1,9 +1,8 @@
 import os
-from collections import namedtuple
+from collections import Counter, namedtuple
 import re
 from typing import List, Tuple, Dict
 from functools import cache
-import cProfile
 import time
 
 DAY = os.path.basename(__file__)[3:5]
@@ -103,8 +102,36 @@ def most_geodes(bp: Blueprint, state: State, mins_left: int) -> int:
     if mins_left == 0:
         return state.geo
 
-    ns = next_states(bp, state)
+    if mins_left == 1:
+        return state.geo + state.geo_robots
 
+    if mins_left == 2:
+        # either you make one robot and then it makes one extra on the last minute, or you
+        # don't and you just get what you have
+        can_build_bot = state.ore >= bp.geo_cost_ore and state.obs >= bp.geo_cost_obs
+        return state.geo + (2 * state.geo_robots) + (1 if can_build_bot else 0)
+
+    if mins_left == 3:
+        # either you build a robot on 1, on 2, both, or neither
+        can_build_now = state.ore >= bp.geo_cost_ore and state.obs >= bp.geo_cost_obs
+        if can_build_now:
+            can_build_2 = (state.ore + state.ore_robots) >= 2 * bp.geo_cost_ore and (
+                state.obs + state.obs_robots
+            ) >= 2 * bp.geo_cost_obs
+            if can_build_2:
+                return state.geo + (3 * state.geo_robots) + 3
+
+            return state.geo + (3 * state.geo_robots) + 2
+
+        can_build_next = (state.ore + state.ore_robots) >= bp.geo_cost_ore and (
+            state.obs + state.obs_robots
+        ) >= bp.geo_cost_obs
+        if can_build_next:
+            return state.geo + (3 * state.geo_robots) + 1
+
+        return state.geo + (3 * state.geo_robots)
+
+    ns = next_states(bp, state)
     return max(most_geodes(bp, s, mins_left - 1) for s in ns)
 
 
@@ -117,7 +144,9 @@ def run(input):
     x = 0
     for bp in bps:
         print(f"{bp.i}: ", end="", flush=True)
-        m = most_geodes(bp, init_state, 19)
+        m = most_geodes(bp, init_state, 22)
+        most_geodes.cache_clear()
+        next_states.cache_clear()
         print(f"{m}")
         x += bp.i * m
     end = time.time()
@@ -127,16 +156,18 @@ def run(input):
 # 2136 too low
 # took a fucking hour to run
 
+# finally 2301, and only took 230s
+
 
 def run2(input):
+    bps = [parse_line(line) for line in input.split("\n")]
     print(2)
 
 
 print("=" * 40)
-run(sample_input)
-# cProfile.run("run(sample_input)")
+# run(sample_input)
 # run(real_input)
 
 # print()
-# run2(sample_input)
+run2(sample_input)
 # run2(real_input)
