7f6ecb2e29adb368d8f08a546e3da322eef23a25
David Crespo <david.crespo@oxidecomputer.com>
2022-12-27 23:39:47 -0500

    2022 day 16 part 1 python

--- 2022/python/day16.py
+++ 2022/python/day16.py
@@ -1,51 +1,41 @@
 import os
+import re
+import time
 
-# import util as u
-# from grid import Grid, pp, parse
-# from parse import parse
-from collections import namedtuple, defaultdict, Counter
-
-# from dataclasses import dataclass
 from functools import cache
-
-# import math
-from typing import List, Tuple, Dict
+from typing import List, Tuple
 
 DAY = os.path.basename(__file__)[3:5]
-with open(f"input/day{DAY}-py.txt") as f:
+with open(f"input/day{DAY}.txt") as f:
     real_input = f.read().strip()
 
-with open(f"input/day{DAY}-py-example.txt") as f:
+with open(f"input/day{DAY}-example.txt") as f:
     s1 = f.read().strip()
-
-
-def parse_input(input):
-    edges, rates = input.split("\n\n")
-    edges = [e.split() for e in edges.split("\n")]
-    edges = [(a, b, int(w)) for a, b, w in edges]
-
-    edg = defaultdict(list)
-    for a, b, w in edges:
-        edg[a].append((b, w))
-
-    rates = [r.split() for r in rates.split("\n")]
-    rates = dict((n, int(w)) for n, w in rates)
-
-    return edg, rates
 
 
 def tup(xs):
     return tuple(sorted(xs))
 
 
+def parse(input):
+    rates = dict()
+    edges = dict()
+    for line in input.split("\n"):
+        src, *tgts = re.findall("[A-Z]{2}", line)
+        edges[src] = tgts
+
+        rate = int(re.search("\d+", line)[0])
+        rates[src] = rate
+
+    return edges, rates
+
+
 def run(input):
-    edges, rates = parse_input(input)
+    start = time.time()
 
-    # print(edges)
-    # print()
-    # print(rates)
+    edges, rates = parse(input)
 
-    working_valves = tup(k for k, v in rates.items() if v > 0)
+    working_valves = set(k for k, v in rates.items() if v > 0)
 
     @cache
     def rate(on):
@@ -53,47 +43,46 @@ def run(input):
 
     # off and on are sorted tuples representing sets
     @cache
-    def search(off, on, curr: str, steps_left: int) -> Tuple[int, List[str]]:
+    def search(on, curr: str, steps_left: int) -> Tuple[int, List[str]]:
         if steps_left == 0:
             return (0, [])
 
         rat = rate(on)
 
-        if off == ():
+        if set(on) == working_valves:
             return (steps_left * rat, [])
 
-        options = [(t, w) for t, w in edges[curr] if w <= steps_left]
+        options = edges[curr]
+
+        # if curr is a working valve and not currently on, then we need an
+        # option to stay here and turn it on
+        if curr in working_valves and curr not in on:
+            options.append(curr)
 
         if not options:
             return (steps_left * rat, [])
 
-        def run(n, w):
-            m, path = search(
-                # I think these are why my answer is too high. this makes it so
-                # the valve goes into effect immediately. It needs to go into
-                # effect one step after being turned on. this is pretty tough
-                # with the abridged graph. turns out with all this memoization I
-                # can probably just use the original and not even bother with
-                # the A* BS
-                tup(set(off) - set([curr])),
-                tup(set([*on, curr])),
-                n,
-                steps_left - w,
-            )
-            return (rat * w + m, path + [n])
+        def run(n):
+            new_on = tup(set([*on, curr])) if n == curr else on
+            m, path = search(new_on, n, steps_left - 1)
+            return (rat + m, [n] + path)
 
-        return max((run(n, w) for n, w in options), key=lambda p: p[0])
+        return max(map(run, options), key=lambda p: p[0])
 
-    print(search(working_valves, (), "AA", 30))
+    mins = 30
+    print(f"mins: {mins}")
+    print(search((), "AA", mins))
+    end = time.time()
+    print(f"time: {round(end-start, 1)}s")
 
 
 def run2(input):
-    inp = parse_input(input)
+    pass
 
 
 print("=" * 40)
-run(s1)
-# run(real_input)
+# run(s1)
+run(real_input)  # took 7 minutes
 
 # print()
 # run2(s1)
